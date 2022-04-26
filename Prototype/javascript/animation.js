c574aa89dc5935457ee4f5eaca72e0224548aa0f
afl300 <aflstudio1@outlook.com.au>
2022-04-26 13:42:14 +1000

    Make collapsibles that is animated
    Added temporary style in (this is still the CSS is created)
    Javascript code updated so blocks can be animated

--- Prototype/javascript/animation.js
+++ Prototype/javascript/animation.js
@@ -5,15 +5,14 @@ function collapsibles()
 {
 	var coll = document.getElementsByClassName("collapsible");
 	var i;
-	
 	for (i = 0; i < coll.length; i++) {
 		coll[i].addEventListener("click", function() {
 			this.classList.toggle("active");
 			var content = this.nextElementSibling;
-			if (content.style.display === "block") {
-				content.style.display = "none";
+			if (content.style.maxHeight){
+				content.style.maxHeight = null;
 			} else {
-				content.style.display = "block";
+				content.style.maxHeight = content.scrollHeight + "px";
 			}
 		});
 	}
