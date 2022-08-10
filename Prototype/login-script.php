e64b063a60e197f185a235c321d6f11847f9b776
Aethalis <icelasersparr@gmail.com>
2022-08-10 10:05:43 +1000

    Now using sessions for PHP persistent variable storage, and moved styles to style.cc and JS to login-js.php

--- Prototype/login-script.php
+++ Prototype/login-script.php
@@ -2,6 +2,9 @@
 //      https://www.w3schools.com/php/php_form_url_email.asp
 
 <?php
+    session_start();
+    $_SESSION['id'] = '123';
+
     function validEmail($email)
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL);
@@ -12,48 +15,28 @@
         return !preg_match("/^[a-zA-Z-' ]*$/",$name);
     }
 
-    function checkEmpty($arr)
-    {
-        $ret = true;
-
-        if (is_array($arr))
-        {
-            for($x = 0; $x < count($arr) || $ret; $x++)
-            {
-                $ret &= empty($arr[$x]);
-            }
-        }
-        else
-        {
-            $ret = empty($arr);
-        }
-
-        return $ret;
-    }
-
-    echo '<p>test</p>';
-
     if (isset($_POST["login-submit"]))
     {
-         echo "3";
         // Get variables
-        $email = $_POST["email"];
-        $pwd = $_POST["password"];
+        $email = $_POST["login-email"];
+        $pwd = $_POST["login-password"];
+
+        $_SESSION["login-email"] = $email;
 
         // Validate variables
-        if (checkEmpty([$email, $pwd]))
+        if (empty($email) || empty($pwd))
         {
-            header("Location: login.php?login=empty&email=$email");
+            header("Location: login.php?login=empty");
             exit();
         }
         elseif (!validEmail($email))
         {
-            header("Location: login.php?login=email&email=$email");
+            header("Location: login.php?login=email");
             exit();
         }
         else
         {
-            header("Location: login.php?login=success&email=$email");
+            header("Location: login.php?login=success");
             exit();
         }
     }
--- Prototype/login.php
+++ Prototype/login.php
@@ -1,159 +1,14 @@
+<?php
+    session_start();
+    $_SESSION['id'] = '123';
+?>
+
 // Example modified from
 <!DOCTYPE html>
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            body, p
-            {
-                font-family: "Times New Roman", Helvetica, sans-serif;
-            }
-
-            .modal-overlay
-            {
-                display: none; /* Hidden by default */
-                position: fixed; /* Stay in place */
-                z-index: 1; /* Sit on top */
-                padding-top: 100px; /* Location of the box */
-                left: 0;
-                top: 0;
-                width: 100%; /* Full width */
-                height: 100%; /* Full height */
-                overflow: auto; /* Enable scroll if needed */
-                background-color: rgb(0,0,0); /* Fallback color */
-                background-color: rgba(0,0,0,0.6); /* Black w/ opacity */
-            }
-
-            .modal-content
-            {
-                /* background-color: #fefefe; */
-                background-image:
-                linear-gradient(to bottom, rgba(255,255,255,0.44), rgba(255,255,255,0.44)),
-                url('img/backgrounds/login-background.jpg');
-                background-size: cover;
-                margin: auto;
-                padding: 20px;
-                border: 1px solid #888;
-                width: 500px;
-            }
-
-            .close-btn
-            {
-                color: #aaaaaa;
-                float: right;
-                font-size: 28px;
-                font-weight: bold;
-            }
-
-            .close-btn:hover, .close-btn:focus
-            {
-                color: #000;
-                text-decoration: none;
-                cursor: pointer;
-            }
-
-            .centre-text
-            {
-                text-align: center;
-            }
-
-            .centre-element
-            {
-                position: relative;
-                left: 50%;
-                top: 50%;
-                transform: translate(-50%, -50%);
-            }
-
-            .decorated-lines
-            {
-                position: relative;
-                z-index: 1;
-                overflow: hidden;
-                text-align: center;
-            }
-
-            .decorated-lines:before, .decorated-lines:after {
-                position: absolute;
-                top: 51%;
-                overflow: hidden;
-                width: 50%;
-                height: 1px;
-                content: '\a0';
-                background-color: black;
-            }
-
-            .decorated-lines:before
-            {
-                margin-left: -50%;
-            }
-
-            .no-text-decoration
-            {
-                text-decoration: none;
-            }
-
-            .no-vertical-margins
-            {
-                margin-top: 0px;
-            }
-
-            .no-vertical-padding
-            {
-                padding-top: 0px;
-                padding-bottom: 0px;
-            }
-
-            input, button
-            {
-                font-size: 20px;
-            }
-
-            input::placeholder
-            {
-                text-align: center;
-            }
-
-            .input-window-format
-            {
-                width: 75%;
-                height: 40px;
-                border: none;
-            }
-
-            .modal-content-header
-            {
-                font-size: 30px;
-            }
-
-            .modal-btn
-            {
-                width: 60%;
-                height: 35px;
-                border: none;
-                display: inline-block;
-            }
-
-            .login-btn
-            {
-                background-color: #172026;
-                color: white;
-            }
-
-            .create-account-option-btn
-            {
-                background-color: #eaf7f6;
-                color: #172026;
-            }
-
-            .error
-            {
-                color: red;
-                position: relative;
-                transform: translate(12%, -15%);
-            }
-
-        </style>
+        <link rel="stylesheet" href="style/style.css">
     </head>
     <body>
         <h2>Modal Example</h2>
@@ -182,30 +37,26 @@
                         {
                             echo '<p class="error">Invalid email</p>';
                         }
-                        elseif ($loginVar == "success")
-                        {
-                            echo '<p class="error">Login success</p>';
-                        }
                     }
                 ?>
-                <form action="login-script.php" method="post">
+                <form action="login-script.php" method="POST">
                     <input
-                        name="email"
+                        name="login-email"
                         class="centre-element input-window-format"
                         type="text"
                         placeholder="EMAIL ADDRESS"
                         style="margin-bottom: 7px;"
                         <?php
-                            if (isset($_GET["email"]))
+                            if (isset($_SESSION["login-email"]))
                             {
-                                $email = $_GET["email"];
-                                echo "value=$email";
+                                $email = $_SESSION["login-email"];
+                                echo "value='$email'";
                             }
                         ?>
                     />
                     <br>
                     <input
-                        name="password"
+                        name="login-password"
                         class="centre-element input-window-format"
                         type="password"
                         placeholder="PASSWORD"
@@ -224,29 +75,54 @@
             <div class="modal-content">
                 <span class="close-btn">&times;</span>
                 <p class="centre-text modal-content-header">CREATE AN ACCOUNT</p>
-                <form action="create-account.php" method="post">
+                <form action="create-account-script.php" method="post">
                     <input
+                        name="ca-first-name"
                         class="centre-element input-window-format"
                         type="text"
                         placeholder="FIRST NAME"
                         style="margin-bottom: 7px;"
+                        <?php
+                        if (isset($_SESSION["ca-first-name"]))
+                        {
+                            $first_name = $_SESSION["ca-first-name"];
+                            echo "value='$first_name'";
+                        }
+                        ?>
                     />
                     <br>
                     <input
+                        name="ca-last-name"
                         class="centre-element input-window-format"
                         type="text"
                         placeholder="LAST NAME"
                         style="margin-bottom: 7px;"
+                        <?php
+                        if (isset($_SESSION["ca-last-name"]))
+                        {
+                            $last_name = $_SESSION["ca-last-name"];
+                            echo "value='$last_name'";
+                        }
+                        ?>
                     />
                     <br>
                     <input
+                        name="ca-email"
                         class="centre-element input-window-format"
                         type="text"
                         placeholder="EMAIL ADDRESS"
                         style="margin-bottom: 7px;"
+                        <?php
+                        if (isset($_SESSION["ca-email"]))
+                        {
+                            $email = $_SESSION["ca-email"];
+                            echo "value='$email'";
+                        }
+                        ?>
                     />
                     <br>
                     <input
+                        name="ca-password"
                         class="centre-element input-window-format"
                         type="password"
                         placeholder="PASSWORD"
@@ -254,6 +130,7 @@
                     />
                     <br>
                     <input
+                        name="ca-password-confirm"
                         class="centre-element input-window-format"
                         type="password"
                         placeholder="CONFIRM PASSWORD"
@@ -261,59 +138,35 @@
                     />
                     <br>
                     <label for="privacy-policy-checkbox" style="position: relative; left: 12%;">
-                        <input type="checkbox"/> I agree to the <a href="https://www.blank.org">privacy policy</a>
+                        <input name="ca-privacy-policy" type="checkbox"/> I agree to the <a href="https://www.blank.org">privacy policy</a>
                     </label>
                     <br>
-                    <input type="submit" name="create-account-submit" class="centre-element modal-btn login-btn" style="position: relative; transform: translate(-50%, 15px)" value="LOGIN">
+                    <button type="submit" name="ca-submit" class="centre-element modal-btn login-btn" style="position: relative; transform: translate(-50%, 15px)">LOGIN</button>
                 </form>
             </div>
         </div>
-
+        <script src="scripts/login-js.php"></script>
         <script>
-            // Get modals
-            var login_modal = document.getElementById("login-overlay");
-            var create_account_modal = document.getElementById("create-account-overlay");
-
-            // Get popup buttons
-            var login_popup_button = document.getElementById("login-launch-btn");
-            var create_account_popup_button = document.getElementById("create-account-launch-btn");
-
-            var login_create_account_button = document.getElementById("login-create-account-btn");
-
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close-btn");
-
-            // When the user clicks the button, open the modal
-            login_popup_button.onclick = function()
-            {
-                login_modal.style.display = "block";
-            }
-
-            create_account_popup_button.onclick = function()
-            {
-                create_account_modal.style.display = "block";
-            }
-
-            login_create_account_button.onclick = function()
-            {
-                login_modal.style.display = "none";
-                create_account_modal.style.display = "block";
-            }
-
-            for (let i = 0; i < span.length; i++)
-            {
-                // When the user clicks on <span> (x), close the modal
-                span[i].onclick = function()
-                {
-                    login_modal.style.display = "none";
-                    create_account_modal.style.display = "none";
-                }
-            }
-
             <?php
                 if (isset($_GET["login"]))
                 {
-                    echo 'login_modal.style.display = "block";';
+                    $loginVar = $_GET["login"];
+
+                    if ($loginVar != "success")
+                    {
+                        echo 'var login_modal = document.getElementById("login-overlay");';
+                        echo 'login_modal.style.display = "block";';
+                    }
+                }
+                elseif (isset($_GET["ca"]))
+                {
+                    $caVar = $_GET["ca"];
+
+                    if ($caVar != "success")
+                    {
+                        echo 'var create_account_modal = document.getElementById("create-account-overlay");';
+                        echo 'create_account_modal.style.display = "block";';
+                    }
                 }
             ?>
         </script>
