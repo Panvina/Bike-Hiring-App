c50164f84a91c0534ed792f0642ba5fcd8846bda
aadesh-j <62002089+aadesh-j@users.noreply.github.com>
2022-08-11 00:51:39 +1000

    Revamped and simplified all styling
    - Converted all files to PHP
    - Styling revamped on all 3 pages extensively
    - Removed repetitive divs to simplify code

---
+++ Prototype/Explore.php
@@ -0,0 +1,38 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Explore</title>
+</head>
+<body>
+    <div id = "main">
+        <div class="banner">
+            <div id="bannertext">
+                <h1> EXPLORE</h1>
+            </div>
+        </div>
+        
+        <div class='ExploreMainCont'>  
+            <div class='ExploreImg'><img style= 'height: 100%; width: 100%; 'src="./img/photos/2.jpg" alt="About Us"/></div>
+            <div class='ExploreTxt'><h1 id="ExploreHeader">CYCLING IN OUR REGION</h1>
+                <div class='ExploreDesc'><p>Discover sights unseen from the highways, and fill your basket with gourmet treats from small producers all across Gippsland. Sit a while on riverbanks and seashores, and experience the beauty of the peaceful surroundings.</p>
+                <a href = "About.php"><button class="button">Learn More</button></a>
+                </div>  
+            </div>
+        </div>
+
+        <div class='ExploreMainCont'>
+            <div class='ExploreTxt'><h1 id="ExploreHeader">RAIL TRAILS</h1>
+                <div class='ExploreDesc'><p>Cycle your way across Gippsland on the rail trails – a network of disused railways transformed into gently graded paths through bushland and rainforest, past valleys and vineyards, and along the stunning beauty of the coastline. Pedal from town to town, sampling the flavours of the region as you go.</p>
+                <a href = "Explore.php"><button style="padding: bottom 20px;"class="button">Contact Us</button></a>
+                </div>
+            </div>  
+            <div class='ExploreImg'><img style= 'height: 100%; width: 100%; 'src="./img/photos/2.jpg" alt="About Us"/></div>
+        </div> 
+    </div>
+
+</body>
+</html>
