9f784cfd9fb09044eedcbe601cbafa464f13e4ce
Aethalis <icelasersparr@gmail.com>
2022-08-11 17:22:16 +1000

    Created simple database connection and insert to table php file

---
+++ Prototype/backend-connection.php
@@ -0,0 +1,63 @@
+<?php
+	class DBConnection
+	{
+		private $conn = null;
+
+		public function __construct($servername, $username, $password, $dbname)
+		{
+			$this->conn = new mysqli($servername, $username, $password, $dbname);
+		}
+
+		public function __destruct()
+		{
+			$this->conn->close();
+		}
+
+		public function insert($tablename, $columns, $data)
+		{
+			$query = "INSERT INTO $tablename ($columns) VALUES ($data)";
+			echo $query;
+			if ($this->conn->query($query) == TRUE)
+			{
+				echo "Record created successfully";
+			}
+			else
+			{
+				echo "Error: " . $query . "<br>" . $this->conn->error;
+
+			}
+		}
+	}
+
+	$conn = new DBConnection("localhost", "root", "", "bike_hiring_system");
+	$conn->insert("bike_type_table", "Name, Description", "'Hydro', 'Non-existent'");
+
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $dbName = "bike_hiring_system";
+	//
+	// // Create connection
+	// $conn = new mysqli($servername, $username, $password, $dbName);
+	//
+	// // Check connection
+	// if ($conn->connect_error) {
+	// 	die("Connection failed: " . $conn->connect_error);
+	// 	echo "Connection failed";
+	// }
+	// 	echo "Connected successfully";
+	//
+	// $sql = "INSERT INTO bike_type_table (Name, Description)
+	// VALUES ('Electric', 'Bad bikes')";
+	//
+	// if ($conn->query($sql) === TRUE) {
+	//   echo "New record created successfully";
+	// } else {
+	//   echo "Error: " . $sql . "<br>" . $conn->error;
+	// }
+	//
+	// $conn->close();
+?>
+
+<?php
+?>
