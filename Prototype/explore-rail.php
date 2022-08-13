013e220fcf0cb050059f032c7e728ce182be9175
Aethalis <icelasersparr@gmail.com>
2022-08-14 02:10:42 +1000

    ggs

---
+++ Prototype/explore-rail.php
@@ -0,0 +1,85 @@
+﻿<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style/style.css">
+        <title>Explore</title>
+    </head>
+    <body>
+        <div id = "main">
+            <div class="banner">
+                <div id="bannertext">
+                    <h1> EXPLORE</h1>
+                </div>
+            </div>
+            <div class="explore-std-padding" style="padding-left: 2.5%;">
+                <p>
+                    <a href="About.php" class="no-text-decoration">Home</a> > <a href="About.php" class="no-text-decoration">Explore</a> > Rail Trails
+                </p>
+            </div>
+
+            <div class="explore-rail-description explore-std-padding" style="display: block; padding-bottom: 0px;">
+                <h1 class="explore-std-padding" style="padding-left: 2.5%; text-decoration: underline; font-weight: bold;">RAIL TRAILS</h1>
+                <p style="padding-left: 2.5%; padding-right: 2.5%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta laoreet gravida. Morbi et sagittis lorem. Nullam luctus nibh at pharetra tempus. Donec mattis vitae lacus eget placerat. Phasellus elit lorem, pellentesque ut ultricies non, ultrices at leo. Vestibulum convallis erat metus, eleifend ultricies tortor tincidunt in. Vestibulum ac efficitur turpis. Nulla eu mattis tortor. Integer gravida pulvinar neque nec ultricies. Mauris sit amet turpis iaculis, pellentesque tellus sit amet, sagittis odio. Integer ac libero vel justo facilisis congue nec sit amet ligula. Aenean sit amet imperdiet ex. In finibus convallis condimentum. Nam convallis faucibus velit, sed tempus metus tempus sed.</p>
+            </div>
+            <div class="explore-rail-content-row" style="padding-left: 2.5%; padding-right: 2.5%;" >
+                <div class="location-container" style="width: 30%; float: left; padding-left: 7.5%;">
+                    <div class="location-image-container" style="background-image: url('img/photos/1.jpg'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 66.64%; margin-bottom: 0px;"
+                        <a/>
+                    </div>
+                    <div class="location-description" style="background-color: silver; border-style: solid; border-width: 2px; border-top-style: none; margin-top: 0px; padding-left: 1em; padding-top: 1px;">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                    </div>
+                </div>
+
+                <div class="location-container" style="width: 30%; float: right; padding-right: 7.5%;">
+                    <div class="location-image-container" style="
+                        background-image: url('img/photos/1.jpg');
+                        background-size: contain;
+                        background-repeat: no-repeat;
+                        width: 100%;
+                        height: 0;
+                        padding-top: 66.64%;
+                        margin-bottom: 0px;">
+                    </div>
+                    <div class="location-description" style="background-color: silver; border-style: solid; border-width: 2px; border-top-style: none; margin-top: 0px; padding-left: 1em; padding-top: 1px;">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                    </div>
+                </div>
+            </div>
+            <div style="padding-top: 33em; margin-botton: 90em;">
+                <p></p>
+            </div>
+            <div class="explore-rail-content-row" style="padding-left: 2.5%; padding-right: 2.5%;" >
+                <div class="location-container" style="width: 30%; float: left; padding-left: 7.5%;">
+                    <div class="location-image-container" style="background-image: url('img/photos/1.jpg'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 66.64%; margin-bottom: 0px;"
+                        <a/>
+                    </div>
+                    <div class="location-description" style="background-color: silver; border-style: solid; border-width: 2px; border-top-style: none; margin-top: 0px; padding-left: 1em; padding-top: 1px;">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                    </div>
+                </div>
+
+                <div class="location-container" style="width: 30%; float: right; padding-right: 7.5%;">
+                    <div class="location-image-container" style="
+                        background-image: url('img/photos/1.jpg');
+                        background-size: contain;
+                        background-repeat: no-repeat;
+                        width: 100%;
+                        height: 0;
+                        padding-top: 66.64%;
+                        margin-bottom: 0px;">
+                    </div>
+                    <div class="location-description" style="background-color: silver; border-style: solid; border-width: 2px; border-top-style: none; margin-top: 0px; padding-left: 1em; padding-top: 1px;">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
