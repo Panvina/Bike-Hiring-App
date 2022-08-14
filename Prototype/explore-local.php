ac7f0ff955e06fb35a0941baeeb1043c8bcacb0e
Aethalis <icelasersparr@gmail.com>
2022-08-14 13:39:44 +1000

    Finished initial regional and rail trail explore pages

--- Prototype/explore-local.php
+++ Prototype/explore-local.php
@@ -1,38 +1,100 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="./style/style.css">
-    <title>Explore</title>
-</head>
-<body>
-    <div id = "main">
-        <div class="banner">
-            <div id="bannertext">
-                <h1> EXPLORE</h1>
-            </div>
-        </div>
-        
-        <div class='ExploreMainCont'>  
-            <div class='ExploreImg'><img style= 'height: 100%; width: 100%; 'src="./img/photos/2.jpg" alt="About Us"/></div>
-            <div class='ExploreTxt'><h1 id="ExploreHeader">CYCLING IN OUR REGION</h1>
-                <div class='ExploreDesc'><p>Discover sights unseen from the highways, and fill your basket with gourmet treats from small producers all across Gippsland. Sit a while on riverbanks and seashores, and experience the beauty of the peaceful surroundings.</p>
-                <a href = "About.php"><button class="button">Learn More</button></a>
-                </div>  
-            </div>
-        </div>
-
-        <div class='ExploreMainCont'>
-            <div class='ExploreTxt'><h1 id="ExploreHeader">RAIL TRAILS</h1>
-                <div class='ExploreDesc'><p>Cycle your way across Gippsland on the rail trails – a network of disused railways transformed into gently graded paths through bushland and rainforest, past valleys and vineyards, and along the stunning beauty of the coastline. Pedal from town to town, sampling the flavours of the region as you go.</p>
-                <a href = "Explore.php"><button style="padding: bottom 20px;"class="button">Contact Us</button></a>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style/style.css">
+        <title>Explore</title>
+    </head>
+    <body>
+        <div id = "main">
+            <div class="banner">
+                <div id="bannertext">
+                    <h1> EXPLORE</h1>
                 </div>
-            </div>  
-            <div class='ExploreImg'><img style= 'height: 100%; width: 100%; 'src="./img/photos/2.jpg" alt="About Us"/></div>
-        </div> 
-    </div>
+            </div>
+            <div class="explore-std-padding">
+                <p>
+                    <a href="About.php" class="no-text-decoration">Home</a> > <a href="About.php" class="no-text-decoration">Explore</a> > Cycling in our Region
+                </p>
+            </div>
 
-</body>
+            <div class="explore-rail-description explore-std-padding">
+                <h1 class="explore-std-header">CYCLING IN OUR REGION</h1>
+                <p class="no-top-margin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta laoreet gravida. Morbi et sagittis lorem. Nullam luctus nibh at pharetra tempus. Donec mattis vitae lacus eget placerat. Phasellus elit lorem, pellentesque ut ultricies non, ultrices at leo. Vestibulum convallis erat metus, eleifend ultricies tortor tincidunt in. Vestibulum ac efficitur turpis. Nulla eu mattis tortor. Integer gravida pulvinar neque nec ultricies. Mauris sit amet turpis iaculis, pellentesque tellus sit amet, sagittis odio. Integer ac libero vel justo facilisis congue nec sit amet ligula. Aenean sit amet imperdiet ex. In finibus convallis condimentum. Nam convallis faucibus velit, sed tempus metus tempus sed.</p>
+            </div>
+
+            <!-- single-row container  -->
+            <div class="explore-rail-content-row explore-std-padding">
+                <div class="left-location-container">
+                    <!-- <div class="location-image-container" style="background-image: url('img/photos/1.jpg'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 66.64%; margin-bottom: 0px;"
+                        <a/>
+                    </div> -->
+                    <img src="img/photos/1.jpg" class="no-separation" alt="nothing" width="579" height="390">
+                    <div class="location-description">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                        <button class="location-details-button">Details</button>
+                    </div>
+                </div>
+                <div class="right-location-container">
+                    <img src="img/photos/1.jpg" class="no-separation" alt="nothing" width="579" height="390">
+                    <div class="location-description">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                        <button class="location-details-button">Details</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- separator  -->
+            <div class="location-container-separator">
+            </div>
+
+            <!-- single-row container  -->
+            <div class="explore-rail-content-row explore-std-padding">
+                <div class="left-location-container">
+                    <!-- <div class="location-image-container" style="background-image: url('img/photos/1.jpg'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 66.64%; margin-bottom: 0px;"
+                        <a/>
+                    </div> -->
+                    <img src="img/photos/1.jpg" class="no-separation" alt="nothing" width="579" height="390">
+                    <div class="location-description">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                        <button class="location-details-button">Details</button>
+                    </div>
+                </div>
+                <div class="right-location-container">
+                    <img src="img/photos/1.jpg" class="no-separation" alt="nothing" width="579" height="390">
+                    <div class="location-description">
+                        <h3 class="location-container-header">Name Placeholder</h3>
+                        <p class="location-container-content">Location: Placeholder</p>
+                        <button class="location-details-button">Details</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- separator  -->
+            <div class="location-container-separator">
+            </div>
+
+            <div class="explore-std-padding">
+                <img src="img/photos/1.jpg" alt="nothing" width="1000" height="390" style="padding-left: 10%; float: left;">
+                <div style="background-color: #eaf7f6; position: relative; top: 0em; left: 61.8%; width: 30%; height: 24.4em; ">
+                    <div style="margin-left: 10em; transform: translate(0, -15em);">
+                        <h2>Your New Journey?</h2>
+                        <p>Explore the surrounds of our region!</p>
+                        <button>Explore</button>
+                    </div>
+                    <!-- <h2 style="padding-top: 4.2em; padding-left: 3em;">Your New Journey?</h2>
+                    <p style="padding-left: 4.5em;">Explore the surrounds of our region!</p>
+                    <button style="">Explore</button> -->
+                </div>
+            </div>
+
+            <!-- separator  -->
+            <div class="location-container-separator">
+            </div>
+        </div>
+    </body>
 </html>
