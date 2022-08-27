d9866abed84350c6b66da6a5b5743290a97b0138
afl300 <aflstudio1@outlook.com.au>
2022-08-27 22:00:33 +1000

    Able to make it functionally work with this setup
    Completed in this task:
    - add comments in of the process of items
    - put in a dropdown box or another popup for the Delete or Update
    - update the buttons and test if other method works
    Todo:
    - updating it to OOP methods connection
    - need to make it visually similar to template

--- Prototype/AddLocations.php
+++ Prototype/AddLocations.php
@@ -1,20 +1,22 @@
 <!doctype html>
 <?php
 include("backend-connection.php");
-function sanitise_input($data){
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return($data);
-	}
+include("Reusable.php");
 
+//this area is where the data is retrieved
 $nameInput = $_POST["nameInput"];
+$nameInput = sanitise_input($nameInput);
 $addressInput= $_POST["addressInput"];
+$addressInput = sanitise_input($addressInput);
 $suburbInput = $_POST["suburbInput"];
+$suburbInput = sanitise_input($suburbInput);
 $postcodeInput = $_POST["postcodeInput"];
+$postcodeInput = sanitise_input($postcodeInput);
 $dropoffInput = $_POST["dropOffInput"];
+$dropoffValue= isItNull($dropoffInput);
 $pickupInput = $_POST["pickUpInput"];
-echo "$nameInput, $addressInput, $suburbInput, $postcodeInput, $dropoffInput, $pickupInput";
+$pickupValue =isItNull($pickupInput);
+echo "$nameInput, $addressInput, $suburbInput, $postcodeInput, DropOffInput:$dropoffInput, PickUpInput:$pickupInput, DropOffValue: $dropoffValue, PickUpValue: $pickupValue";
 
 $err_msg="";
 //Name
@@ -35,71 +37,65 @@ else if (!preg_match('/^[0-9]{4}$/',$postcodeInput))
 {
 	$err_msg= "The postcode must be a 4-digit number.";
 }
-else if(!(substr($postcodeInput,-4,1)==3 /*&& $state=="VIC"*/)){
+else if(!(substr($postcodeInput,-4,1)==3)){//To show this is from the state of victoria
 	$err_msg .= "<p>The postcode you enetered is not a Victorian postcode.</p>";
 }
 
-
+		//setting up and connecting to DB
 if($err_msg!=""){
 	echo"<p>$err_msg</p>";
 	exit();
 }
-		$note = "Test: ";
-		$host = "localhost";
-		$user = "root";
-		$pwd = "";
-		$sql_db = "bike_hiring_system";
-		$conn = @mysqli_connect
-			("$host",
-			 "$user",
-			 "$pwd",
-			 "$sql_db") 
-			or die
-			('Failed to connect to server');
-		if (!$conn)
-		{
-			$note .= "Connection Failed";
-			exit();
-		}
-		else
-		{
-			$note .= "Connection Passed";
-			//create table if not exists
-			$query = "CREATE TABLE IF NOT EXISTS `location_table` (
-			  `LocationID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			  `Name` varchar(45) NOT NULL,
-			  `Address` varchar(255) NOT NULL,
-			  `Suburb` varchar(45) NOT NULL,
-			  `Post Code` varchar(4) NOT NULL,
-			  `Drop Off Location` tinyint(4) NOT NULL DEFAULT 0,
-			  `Pick Up Location` tinyint(4) NOT NULL DEFAULT 0
-			)";
-			$result = mysqli_query($conn,$query);
-			if($result){//create table successful
-				$note .= ", Create Table Success";
-				$query = "INSERT INTO `location_table` (`Name`, `Address`, `Suburb`, `Post Code`, `Drop Off Location`, `Pick Up Location`) VALUES ( '$nameInput', '$addressInput', '$suburbInput', '$postcodeInput', '0', '0'); ";
-				
-				$insert_result = mysqli_query ($conn, $query);
-				if($insert_result){//insert successful
-					$note .= ", Success Insert";
-					header("location:Locations.php");
-				} else {
-					$note .= ", Insert  unsuccessful ";
-				}
-			} else {
-			$db_msg .= ", Create table operation unsuccessful";
-		}
-		mysqli_close ($conn);					// Close the database connect
-		}
 
+$db_msg = "";
+$host = "localhost";
+$user = "root";
+$pwd = "";
+$sql_db = "bike_hiring_system";
+$conn = @mysqli_connect
+	("$host",
+	 "$user",
+	 "$pwd",
+	 "$sql_db") 
+	or die
+	('Failed to connect to server');
+if (!$conn)
+{
+	$db_msg .= "Connection Failed";
+	exit();
+}
+else
+{
+	$db_msg .= "Connection Passed";
+	//create table if not exists
+	$query = "CREATE TABLE IF NOT EXISTS `location_table` (
+	`LocationID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`Name` varchar(45) NOT NULL,
+	`Address` varchar(255) NOT NULL,
+	`Suburb` varchar(45) NOT NULL,
+	`Post Code` varchar(4) NOT NULL,
+	`Drop Off Location` tinyint(1) NOT NULL DEFAULT 0,
+	`Pick Up Location` tinyint(1) NOT NULL DEFAULT 0
+	)";
+	$result = mysqli_query($conn,$query);
+	if($result){//create table successful
+		$query = "INSERT INTO `location_table` (`Name`, `Address`, `Suburb`, `Post Code`, `Drop Off Location`, `Pick Up Location`) VALUES ( '$nameInput', '$addressInput', '$suburbInput', '$postcodeInput', '$dropoffValue', '$pickupValue'); ";
+		$insert_result = mysqli_query ($conn, $query);
+		if($insert_result){//insert successful
+			$db_msg .= ", Success Insert";
+			header("location:Locations.php");
+		} else {//if insert data into database fail
+			$db_msg .= ", Insert  unsuccessful ";
+		}
+	} else {
+		$db_msg .= ", Create table operation unsuccessful";
+	}
+	mysqli_close ($conn);// Close the database connect
+}
+
+if($db_msg!="")//this is to print if there is any errors
+{
+	echo"<p>$db_msg</p>";
+	exit();
+}
 ?>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-	<h2><?php echo $note?></h2>
-</body>
-</html>
---
+++ Prototype/UpdateLocations.php
@@ -0,0 +1,81 @@
+<?php
+include("Reusable.php");
+include("backend-connection.php");
+
+//Retrieving data
+$LID = $_POST["LID"];
+$LID = sanitise_input($LID);
+$dropoffInput = $_POST["dropOffBox"];
+$dropoffValue= isItNull($dropoffInput);
+$pickupInput = $_POST["pickUpBox"];
+$pickupValue =isItNull($pickupInput);
+
+//test which button press
+if(isset($_POST["deleteLocation"]))
+{
+	echo "<h2>True Delete</h2>";
+}
+if(isset($_POST["updateLocation"]))
+{
+	echo "<h2>True Update</h2>";
+}
+
+//connection related items
+$host = "localhost";
+$user = "root";
+$pwd = "";
+$db_msg = "";
+$sql_db = "bike_hiring_system";
+$conn = @mysqli_connect
+	("$host",
+	 "$user",
+	 "$pwd",
+	 "$sql_db") 
+	or die
+	('Failed to connect to server');
+
+
+if (!$conn)
+{//if connection fails
+	$db_msg .= "Connection Failed, ";
+	exit();
+}
+else
+{
+	//the two isset is to see which function button is clicked
+	if(isset($_POST["deleteLocation"]))//delete a location that is not important
+	{
+		$query = "DELETE FROM `location_table` WHERE `location_table`.`LocationID` = $LID;";
+		$result = mysqli_query($conn,$query);
+		if($result){//Data Deleted Successfully
+			header("location:Locations.php");
+		}
+		else
+		{//Delete data fail
+			$db_msg .= "Deleting Data Failed";
+		}
+	}
+	
+	if(isset($_POST["updateLocation"]))//updating the checkboxes send
+	{
+		$query = "UPDATE `location_table` SET `Drop Off Location` = '$dropoffValue', `Pick Up Location` = '$pickupValue' WHERE `location_table`.`LocationID` = $LID;";
+		$result = mysqli_query($conn,$query);
+		if($result)
+		{//Data Updated Successfully
+			header("location:Locations.php");//this is to redirect if success
+		}
+		else
+		{//Data Update Fail
+			$db_msg .="Data has fail to update";
+		}
+	}
+	
+	mysqli_close ($conn);// Close the database connect
+}
+
+if($db_msg!="")//this is to print if there is any errors
+{
+	echo"<p>$db_msg</p>";
+	exit();
+}
+?>
