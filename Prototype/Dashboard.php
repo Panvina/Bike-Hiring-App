f250b19d9a602041ac76c0d6db29c276fab1e4fc
afl300 <aflstudio1@outlook.com.au>
2022-09-06 22:52:11 +1000

    SoftCode Calendar initial version put in

--- Prototype/Dashboard.php
+++ Prototype/Dashboard.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <link rel="stylesheet" href="style/Jake_style.css">
+    <link rel="stylesheet" href="style/scheduleTimetableStyle.css">
     <head>
         <!-- header -->
         <title> Admin dashboard </title>
@@ -12,7 +13,6 @@
             <div class = "sideNavigation">
                 <a class="active" href= "Dashboard.php"> <img src= "img/icons/bulletin-board.png" alt="Dashboard Logo" /> Dashboard </a> <br>
                 <a href = "Customer.php"> <img src= "img/icons/account-group.png" alt="Customer Logo" />  Customer  </a> <br>
-                <a href="staff.php"> <img src="img/icons/staff.png" alt="Staff Logo" /> Staff </a> <br>
                 <a href= "Inventory.php"> <img src= "img/icons/bicycle.png" alt="Inventory Logo" />  Inventory </a> <br>
                 <a href= "bookings.php"> <img src= "img/icons/book-open-blank-variant.png" alt="Bookings Logo" /> Bookings </a> <br>
                 <a href= "Block_Out_Date.php"> <img src= "img/icons/calendar.png" alt="Block out date Logo" /> Block Out Dates </a> <br>
@@ -57,7 +57,52 @@
             <br>
 
             <!-- Current bookings section -->
-            <h1 style="line-height: 100px;"> Current Bookings: </h1>
+            <h1 style="line-height: 100px;"><strong> Current Bookings: </strong></h1>
+                  <!--            keeping the schedule inside it-->
+                              <div class="scheduleContainer">
+                              <!-- TIMES -->
+<!--
+                    <div class="time start-800">8:00</div>
+                    <div class="time start-830">8:30</div>
+-->
+                    <div class="time start-900">9:00</div>
+                    <div class="time start-930">9:30</div>
+                    <div class="time start-1000">10:00</div>
+                    <div class="time start-1030">10:30</div>
+                    <div class="time start-1100">11:00</div>
+                    <div class="time start-1130">11:30</div>
+                    <div class="time start-1200">12:00</div>
+                    <div class="time start-1230">12:30</div>
+                    <div class="time start-1300">13:00</div>
+                    <div class="time start-1330">13:30</div>
+                    <div class="time start-1400">14:00</div>
+                    <div class="time start-1430">14:30</div>
+                    <div class="time start-1500">15:00</div>
+                    <div class="time start-1530">15:30</div>
+                    <div class="time start-1600">16:00</div>
+                    <div class="time start-1630">16:30</div>
+                    <div class="time start-1700">17:00</div>
+                    <div class="time start-1730">17:30</div>
+                    <div class="time start-1800">18:00</div>
+                    <div class="time start-1830">18:30</div>
+                    <div class="time start-1900">19:00</div>
+                  <!--
+                    <div class="time start-1930">19:30</div>
+                    <div class="time start-2000">20:00</div>
+                    <div class="time start-2030">20:30</div>
+                    <div class="time start-2100">21:00</div>
+                    <div class="time start-2130">21:30</div>
+                  -->
+				  <!-- EVENTS -->
+<!--				  <div class="event stage-earth start-1030 end-1030 length-2">Speaker 6 <span>Earth Stage</span></div>-->
+								  
+				  <div class="event stage-bike start-900 end-1100 length-1">Danny</div>
+				  <div class="event stage-bike start-900 end-1200 length-1">Meika</div>
+				  <div class="event stage-bike start-1100 end-1400 length-1">Jessica</div>
+				  <div class="event stage-bike start-1100 end-1200 length-1">Daniel</div>
+				  <div class="event stage-bike start-1200 end-1500 length-1">Callum</div>
+				  <div class="event stage-bike start-1400 end-1700 length-1">Rob</div>
+			</div>
             <div>
 
             </div>
