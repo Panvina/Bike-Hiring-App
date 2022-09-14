56cfe5da1070b2d7caaa1432d0a0b24097a5ab11
afl300 <aflstudio1@outlook.com.au>
2022-09-14 19:32:08 +1000

    Update Dashboard.php

--- Prototype/Dashboard.php
+++ Prototype/Dashboard.php
@@ -1,3 +1,14 @@
+<!--
+Project Name: Inverloch Bike Hire
+Project Description: A website for hiring bikes. Front-end accompanied
+	   by an admin dashboard.
+File Description: Show both the functions and locations information
+Contributor(s):
+	- Clement Cheung @ 103076376@student.swin.edu.au
+	- Vina Touch @ 101928802@student.swin.edu.au
+	- Jake Hipworth @ 102090870@student.swin.edu.au (Navigation section and Styles)
+	- Dabin Lee @ icelasersparr@gmail.com
+-->
 <?php
     session_start();
 
@@ -21,6 +32,7 @@
 <!--DOCTYPE html -->
 <html>
     <link rel="stylesheet" href="style/Jake_style.css">
+    <link rel="stylesheet" href="style/scheduleTimetableStyle.css">
     <head>
         <!-- header -->
         <title> Admin dashboard </title>
@@ -115,8 +127,111 @@
             </div>
             <br>
 
+            <!--This calendar section is done by Clement where it is showing the current bookings as an event scheduler and it is hard code since it is to demonstrate the potential type of thing-->
             <!-- Current bookings section -->
-            <h1 style="line-height: 100px;"> Current Bookings: </h1>
+            <h1 style="line-height: 50px;"><strong> Current Bookings: </strong></h1>
+			<div class="keyContainer"><div class="keys">Key:</div> 
+				<div class="event stage-booked length-1 key">Booked</div>
+				<div class="event stage-confirm length-1 key">Confirm</div>
+				<div class="event stage-checkedOut length-1 key">Checkout</div>
+				<div class="event stage-provisional length-1 key">Provisional</div>
+				
+			</div>
+			<div class="background">
+			<pre>
+			<!--keeping the schedule inside it-->
+			<div class="scheduleContainer">
+				<!-- TIMES -->
+				<div class="time start-900">9:00</div>
+				<div class="time start-930">9:30</div>
+				<div class="time start-1000">10:00</div>
+				<div class="time start-1030">10:30</div>
+				<div class="time start-1100">11:00</div>
+				<div class="time start-1130">11:30</div>
+				<div class="time start-1200">12:00</div>
+				<div class="time start-1230">12:30</div>
+				<div class="time start-1300">13:00</div>
+				<div class="time start-1330">13:30</div>
+				<div class="time start-1400">14:00</div>
+				<div class="time start-1430">14:30</div>
+				<div class="time start-1500">15:00</div>
+				<div class="time start-1530">15:30</div>
+				<div class="time start-1600">16:00</div>
+				<div class="time start-1630">16:30</div>
+				<div class="time start-1700">17:00</div>
+                  
+				<!-- EVENTS -->				
+				<!--This is to sort in order of time start so the it can be shown cleanly-->
+				
+				
+				<!--Given the scenario where there is a lot of booking on this day-->
+				<div class="event stage-checkedOut start-900 end-1700 length-1">Joshua<br/>9:00-<br/>17:00</div>
+				<div class="event stage-checkedOut start-900 end-930 length-1">Josh<br/>9:00-<br/>9:30</div>
+				<div class="event stage-provisional start-900 end-1000 length-1">Cynthia<br/>9:00-<br/>10:00</div>
+				<div class="event stage-checkedOut start-900 end-1030 length-1">Carmen<br/>9:00-<br/>10:30</div>
+				<div class="event stage-checkedOut start-900 end-1100 length-1">Danny<br/>9:00-<br/>11:00</div>
+				<div class="event stage-checkedOut start-900 end-1130 length-1">David<br/>9:00-<br/>11:30</div>
+				<div class="event stage-checkedOut start-900 end-1130 length-1">Jonothan<br/>9:00-<br/>11:30</div>
+				<div class="event stage-checkedOut start-900 end-1700 length-1">Joshua<br/>9:00-<br/>17:00</div>
+				<div class="event stage-checkedOut start-900 end-930 length-1">Josh<br/>9:00-<br/>9:30</div>
+				<div class="event stage-provisional start-900 end-1000 length-1">Cynthia<br/>9:00-<br/>10:00</div>
+				<div class="event stage-checkedOut start-900 end-1030 length-1">Carmen<br/>9:00-<br/>10:30</div>
+				<div class="event stage-checkedOut start-900 end-1100 length-1">Danny<br/>9:00-<br/>11:00</div>
+				<div class="event stage-checkedOut start-900 end-1130 length-1">David<br/>9:00-<br/>11:30</div>
+				<div class="event stage-checkedOut start-900 end-1130 length-1">Jonothan<br/>9:00-<br/>11:30</div>
+				<div class="event stage-provisional start-1330 end-1400 length-1">Mike<br/>13:30-<br/>14:00</div>
+				<div class="event stage-checkedOut start-930 end-1200 length-1">Meika<br/>9:30-<br/>12:00</div>
+				<div class="event stage-checkedOut start-930 end-1200 length-1">Meika<br/>9:30-<br/>12:00</div>
+				<div class="event stage-checkedOut start-1000 end-1030 length-1">Rick<br/>10:00-<br/>10:30</div>
+				<div class="event stage-confirm start-1030 end-1200 length-1">Daniel<br/>10:30-<br/>12:00</div>
+				<div class="event stage-checkedOut start-1000 end-1030 length-1">Rick<br/>10:00-<br/>10:30</div>
+				<div class="event stage-confirm start-1030 end-1200 length-1">Daniel<br/>10:30-<br/>12:00</div>
+				<div class="event stage-confirm start-1100 end-1400 length-1">Jessica<br/>11:00-<br/>14:00</div>
+				<div class="event stage-confirm start-1100 end-1400 length-1">Jessica<br/>11:00-<br/>14:00</div>
+				<div class="event stage-booked start-1430 end-1630 length-1">Jane<br/>14:30-<br/>16:30</div>
+				<div class="event stage-confirm start-1200 end-1300 length-1">Duncan<br/>12:00-<br/>13:00</div>
+				<div class="event stage-provisional start-1200 end-1230 length-1">John<br/>12:00-<br/>12:30</div>
+				<div class="event stage-confirm start-1230 end-1300 length-1">Albert<br/>12:30-<br/>13:00</div>
+				<div class="event stage-provisional start-1200 end-1230 length-1">Billy<br/>12:00-<br/>12:30</div>
+				<div class="event stage-confirm start-1200 end-1300 length-1">Duncan<br/>12:00-<br/>13:00</div>
+				<div class="event stage-provisional start-1200 end-1230 length-1">John<br/>12:00-<br/>12:30</div>
+				<div class="event stage-confirm start-1230 end-1300 length-1">Albert<br/>12:30-<br/>13:00</div>
+				<div class="event stage-provisional start-1200 end-1230 length-1">Billy<br/>12:00-<br/>12:30</div>
+				<div class="event stage-booked start-1300 end-1500 length-1">Bill<br/>13:00-<br/>15:00</div>
+				<div class="event stage-booked start-1300 end-1400 length-1">Roger<br/>13:00-<br/>14:00</div>
+				<div class="event stage-booked start-1300 end-1330 length-1">Daniella<br/>13:00-<br/>14:30</div>
+				<div class="event stage-booked start-1300 end-1400 length-1">Courtney<br/>13:00-<br/>14:00</div>
+				<div class="event stage-confirm start-1300 end-1500 length-1">Callum<br/>13:00-<br/>15:00</div>
+				<div class="event stage-booked start-1300 end-1500 length-1">Bill<br/>13:00-<br/>15:00</div>
+				<div class="event stage-booked start-1300 end-1400 length-1">Roger<br/>13:00-<br/>14:00</div>
+				<div class="event stage-booked start-1300 end-1330 length-1">Daniella<br/>13:00-<br/>14:30</div>
+				<div class="event stage-booked start-1300 end-1400 length-1">Courtney<br/>13:00-<br/>14:00</div>
+				<div class="event stage-confirm start-1300 end-1500 length-1">Callum<br/>13:00-<br/>15:00</div>
+				<div class="event stage-booked start-1330 end-1530 length-1">Ash<br/>13:30-<br/>15:30</div>
+				<div class="event stage-confirm start-1330 end-1400 length-1">Mike<br/>13:30-<br/>14:00</div>
+				<div class="event stage-booked start-1330 end-1530 length-1">Ash<br/>13:30-<br/>15:30</div>
+				<div class="event stage-booked start-1400 end-1500 length-1">Donald<br/>14:00-<br/>15:00</div>
+				<div class="event stage-booked start-1400 end-1500 length-1">Donald<br/>14:00-<br/>15:00</div>
+				<div class="event stage-confirm start-1430 end-1630 length-1">Morty<br/>14:30-<br/>16:30</div>
+				<div class="event stage-booked start-1430 end-1530 length-1">Trent<br/>14:30-<br/>15:30</div>
+				<div class="event stage-provisional start-1430 end-1700 length-1">Rob<br/>14:30-<br/>17:00</div>
+				<div class="event stage-confirm start-1430 end-1630 length-1">Morty<br/>14:30-<br/>16:30</div>
+				<div class="event stage-booked start-1430 end-1530 length-1">Trent<br/>14:30-<br/>15:30</div>
+				<div class="event stage-provisional start-1430 end-1700 length-1">Rob<br/>14:30-<br/>17:00</div>
+				<div class="event stage-booked start-1430 end-1630 length-1">Jane<br/>14:30-<br/>16:30</div>
+				<div class="event stage-provisional start-1530 end-1630 length-1">Bobby<br/>15:30-<br/>16:30</div>
+				<div class="event stage-provisional start-1530 end-1700 length-1">Dave<br/>15:30-<br/>17:00</div>
+				<div class="event stage-confirm start-1600 end-1630 length-1">Steve<br/>16:00-<br/>16:30</div>
+				<div class="event stage-confirm start-1630 end-1700 length-1">Dean<br/>16:30-<br/>17:00</div>
+				<div class="event stage-provisional start-1530 end-1630 length-1">Bobby<br/>15:30-<br/>16:30</div>
+				<div class="event stage-provisional start-1530 end-1700 length-1">Dave<br/>15:30-<br/>17:00</div>
+				<div class="event stage-confirm start-1600 end-1630 length-1">Steve<br/>16:00-<br/>16:30</div>
+				<div class="event stage-confirm start-1630 end-1700 length-1">Dean<br/>16:30-<br/>17:00</div>
+			</div>
+        </pre>
+				
+		</div>
+<!--
             <div>
                 <table>
                     <?php
@@ -175,6 +290,7 @@
                     ?>
                 </table>
             </div>
+-->
         </div>
 
     </body>
