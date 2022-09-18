20334dacbabffa6a335393dd728bb618a3ca4976
102093549 <62001169+102093549@users.noreply.github.com>
2022-09-18 23:24:16 +1000

    Add files via upload

--- Prototype/Block_Out_Date.php
+++ Prototype/Block_Out_Date.php
@@ -17,6 +17,7 @@ $conn = new mysqli("localhost", "root", "", "bike_hiring_system");
 <!DOCTYPE html>
 <html>
     <link rel="stylesheet" href="style/Jake_style.css">
+    <link rel="stylesheet" href="style/blockoutdates.css">
     <head>
          <!-- Header -->
         <title> Block Out Dates </title>
@@ -25,109 +26,13 @@ $conn = new mysqli("localhost", "root", "", "bike_hiring_system");
 
     </head>
 
-    <style type="text/css">
-       #dateContainer{
-    box-sizing: border-box;
-}
-
-
-ul {list-style-type: none;}
-
-
-.month {
-  padding: 10px 25px;
-  text-align: center;
-  color:black;
-  border-top:1px solid black;
-  border-left:1px solid black;
-  border-right:1px solid black;
-}
-
-.month ul {
-  margin: 0;
-  padding: 0;
-  color:black;
-}
-
-.month ul li {
-  color: black;
-  font-size: 20px;
-}
-
-.month .prev {
-  float: left;
-  padding-top: 10px;
-}
-
-.month .next {
-  float: right;
-  padding-top: 10px;
-}
-
-.weekdays {
-  margin: 0;
-  padding: 10px 0;
-  background-color: white;
-  padding-left: 10px;
-  border-left:1px solid black;
-  border-right:1px solid black;
-}
-
-.weekdays li {
-  display: inline-block;
-  width: 13.7%;
-  color: black;
-  text-align: center;
-}
-
-.days {
-  padding: 10px 0;
-  background: white;
-  margin: 0;
-  padding-left: 10px;
-  border-left:1px solid black;
-  border-right:1px solid black;
-  border-bottom:1px solid black;
-}
-
-.days li {
-  list-style-type: none;
-  display: inline-block;
-  width: 14%;
-  text-align: center;
-  margin-bottom: 5px;
-  font-size:12px;
-  color: black;
-}
-
-.days li .blockout {
-  color: grey !important
-}
-
-.date {
-  border: none;
-  outline: none;
-  cursor: pointer;
-  padding: 5px;
-}
-
-.active, .date:hover {
-  background-color: #666;
-  color: white;
-}
- 
-
-
-
-
-    </style>
     <body>
         <!-- Side navigation -->
         <nav>
             <div class = "sideNavigation">
                 <a href= "Dashboard.php"> <img src= "img/icons/bulletin-board.png" alt="Dashboard Logo" /> Dashboard </a> <br>
                 <a href = "Customer.php"> <img src= "img/icons/account-group.png" alt="Customer Logo" />  Customer  </a> <br>
-                <?php  setOwnerDashboardPrivilege(); ?>
+                <?php setOwnerDashboardPrivilege(); ?>
             <!--<a href="accounts.php"> <img src="img/icons/account.png" alt="Account logo"/> Accounts </a> <br>-->
                 <a href= "Inventory.php"> <img src= "img/icons/bicycle.png" alt="Inventory Logo" />  Inventory </a> <br>
                 <a href="Accessory.php"> <img src="img/icons/accessories.png" alt="Inventory Logo" /> Accessories </a> <br>
