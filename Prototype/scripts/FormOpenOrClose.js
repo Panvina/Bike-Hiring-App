ed4ef906cb0ea41fee8e815441e1bb3b7e7dc1f3
afl300 <aflstudio1@outlook.com.au>
2022-09-23 11:17:02 +1000

    Cleaning Up Files and formating code to make it look nicer

--- Prototype/scripts/FormOpenOrClose.js
+++ Prototype/scripts/FormOpenOrClose.js
@@ -5,70 +5,63 @@
 // Contributor:
 // 	- Clement Cheung @ 103076376@student.swin.edu.au
 
-
 // This page is completely done by Clement
-		// Get the modal
-		var addmodal = document.getElementById("addModal");
-		var updateModal = document.getElementById("updateModal");
-		var deleteModal = document.getElementById("deleteModal");
-		
-		// Get the button that opens the modal
-		var addBtn = document.getElementById("addLocationModal");
-		var updateBtn = document.getElementsByClassName("updateLocationModal");
-		var deleteBtn = document.getElementsByClassName("deleteLocationModal");
-		
-		// Get the <span> element that closes the modal
-		var addspan = document.getElementsByClassName("addclose")[0];
-		var updatespan = document.getElementsByClassName("updateclose")[0];
-		var deletespan = document.getElementsByClassName("deleteclose")[0];
-		
-		// When the user clicks on the button, open the modal
-		addBtn.onclick = function() {
-			addmodal.style.display = "block";
-		}
-		
-		for (var i = 0; i < updateBtn.length; i++)
-		{
-			updateBtn[i].onclick = function(event) {
-				updateModal[i].style.display = "block";
-				  }
-		}
+// Get the modal
+var addmodal = document.getElementById("addModal");
+var updateModal = document.getElementById("updateModal");
+var deleteModal = document.getElementById("deleteModal");
 
-		for (var i = 0; i < deleteBtn.length; i++)
-		{
-			deleteBtn[i].onclick = function(event) {
-				deleteModal[i].style.display = "block";
-			}
-		}
-		
-		
-		// When the user clicks on <span> (x), close the modal
-		addspan.onclick = function() {
-			addmodal.style.display = "none";
-			window.location.replace("Locations.php");
-		}
-		updatespan.onclick = function() {
-			updateModal.style.display = "none";
-			window.location.replace("Locations.php?update=false");
-		}
-		deletespan.onclick = function() {
-			deleteModal.style.display = "none";
-			window.location.replace("Locations.php");
-		}
-		
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-			if (event.target == addmodal) {
-				addmodal.style.display = "none";
-				window.location.replace("Locations.php");
-			}
-			else if (event.target == updateModal) {
-				updateModal.style.display = "none";
-				window.location.replace("Locations.php");
-			}
-			else if (event.target == deleteModal) {
-				deleteModal.style.display = "none";
-				window.location.replace("Locations.php");
-			}
-		} 
-		
+// Get the button that opens the modal
+var addBtn = document.getElementById("addLocationModal");
+var updateBtn = document.getElementsByClassName("updateLocationModal");
+var deleteBtn = document.getElementsByClassName("deleteLocationModal");
+
+// Get the <span> element that closes the modal
+var addspan = document.getElementsByClassName("addclose")[0];
+var updatespan = document.getElementsByClassName("updateclose")[0];
+var deletespan = document.getElementsByClassName("deleteclose")[0];
+
+// When the user clicks on the button, open the modal
+addBtn.onclick = function () {
+  addmodal.style.display = "block";
+};
+
+for (var i = 0; i < updateBtn.length; i++) {
+  updateBtn[i].onclick = function (event) {
+    updateModal[i].style.display = "block";
+  };
+}
+
+for (var i = 0; i < deleteBtn.length; i++) {
+  deleteBtn[i].onclick = function (event) {
+    deleteModal[i].style.display = "block";
+  };
+}
+
+// When the user clicks on <span> (x), close the modal
+addspan.onclick = function () {
+  addmodal.style.display = "none";
+  window.location.replace("Locations.php");
+};
+updatespan.onclick = function () {
+  updateModal.style.display = "none";
+  window.location.replace("Locations.php?update=false");
+};
+deletespan.onclick = function () {
+  deleteModal.style.display = "none";
+  window.location.replace("Locations.php");
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == addmodal) {
+    addmodal.style.display = "none";
+    window.location.replace("Locations.php");
+  } else if (event.target == updateModal) {
+    updateModal.style.display = "none";
+    window.location.replace("Locations.php");
+  } else if (event.target == deleteModal) {
+    deleteModal.style.display = "none";
+    window.location.replace("Locations.php");
+  }
+};
--- Prototype/scripts/SendMailTo.js
+++ Prototype/scripts/SendMailTo.js
@@ -8,16 +8,16 @@
 // This page is completely done by Clement
 "use strict";
 
-//This is a security function to make sure that any input, especially hacking code is changed into a safe code. 
+//This is a security function to make sure that any input, especially hacking code is changed into a safe code.
 //idea from https://futurestud.io/tutorials/split-a-string-into-a-list-of-lines-in-javascript-or-node-js#:~:text=You%20can%20split%20a%20long,in%20other%20languages%3A%20%5Cn%20.
 //protection from https://stackoverflow.com/questions/20855482/preventing-html-and-script-injections-in-javascript
 function lines(text) {
   text = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-  return text.split('\n')
+  return text.split("\n");
 }
 
-function sendmail() //this function is to both retrieve, manage data and then send email
-{
+function sendmail() {
+  //this function is to both retrieve, manage data and then send email
   var errMsg = "";
   var result = true;
 
@@ -37,7 +37,8 @@ function sendmail() //this function is to both retrieve, manage data and then se
   if (name == "") {
     errMsg += "The First Name cannot be empty.\n";
   } else if (!name.match(/^[A-Za-z]{1,25}$/)) {
-    errMsg += "The First name need to be in alphabetical order and must be less than 25 characters.\n";
+    errMsg +=
+      "The First name need to be in alphabetical order and must be less than 25 characters.\n";
     result = false;
   }
 
@@ -47,7 +48,7 @@ function sendmail() //this function is to both retrieve, manage data and then se
     result = false;
   } else if (!emailin.match(emailvalidate)) {
     result = false;
-    errMsg += "The email address you entered is invalid.\n"
+    errMsg += "The email address you entered is invalid.\n";
   }
   if (subject == "") {
     errMsg += "The subject cannot be empty. \n";
@@ -64,7 +65,7 @@ function sendmail() //this function is to both retrieve, manage data and then se
     var stringLength = LinesArray.length;
     for (let i = 0; i < stringLength; i++) {
       fullmessage += LinesArray[i];
-      fullmessage += "%0D%0A"
+      fullmessage += "%0D%0A";
     }
   }
 
@@ -77,16 +78,17 @@ function sendmail() //this function is to both retrieve, manage data and then se
   //this is to put message together fully with the sender and email.
   var completemessage = "";
   //https://stackoverflow.com/questions/22765834/insert-a-line-break-in-mailto-body making new lines
-  completemessage = fullmessage + "%0D%0ASender: " + name + "%0D%0AEmail: " + emailin;
+  completemessage =
+    fullmessage + "%0D%0ASender: " + name + "%0D%0AEmail: " + emailin;
 
   //to send the email to the reciever
   if (result) {
-    var sendcompletemail = "mailto:" + emailto + "?subject=" + subject + "&body=" + completemessage;
+    var sendcompletemail =
+      "mailto:" + emailto + "?subject=" + subject + "&body=" + completemessage;
     window.location.href = sendcompletemail;
   }
   return result;
 }
-
 
 //this is operational and for the animation dropdown
 function collapsibles() {
@@ -104,7 +106,6 @@ function collapsibles() {
     });
   }
 }
-
 
 function init() {
   //this is to initiate the email form after it recieve data from contactus
