900aa747c026e237fc9b9ed9329a2130daf01b45
Aethalis <icelasersparr@gmail.com>
2022-10-05 19:19:21 +1100

    Renamed some menu items and changed positioning

--- Prototype/php-scripts/dashboard-menu.php
+++ Prototype/php-scripts/dashboard-menu.php
@@ -32,9 +32,9 @@
         echo "<a class='{$classes['dashboard']}' href= 'Dashboard.php'> <img src= 'img/icons/bulletin-board.png' alt='Dashboard Logo' /> Dashboard </a> <br>";
         echo "<a class='{$classes['customer']}' href='Customer.php'> <img src= 'img/icons/account-group.png' alt='Customer Logo' />  Customer  </a> <br>";
         setOwnerDashboardPrivilege($classes['staff'], $classes['account']);
-        echo "<a class='{$classes['inventory']}' href= 'Inventory.php'> <img src= 'img/icons/bicycle.png' alt='Inventory Logo' />  Inventory </a> <br>";
-        echo "<a class='{$classes['accessory']}' href='Accessory.php'> <img src='img/icons/accessories.png' alt='Inventory Logo' /> Accessories </a> <br>";
+        echo "<a class='{$classes['inventory']}' href= 'Inventory.php'> <img src= 'img/icons/bicycle.png' alt='Inventory Logo' />  Bike Inventory </a> <br>";
         echo "<a class='{$classes['biketype']}' href='BikeTypes.php'> <img src='img/icons/biketypes.png' alt='Bike Types Logo' /> Bike Types </a> <br>";
+        echo "<a class='{$classes['accessory']}' href='Accessory.php'> <img src='img/icons/accessories.png' alt='Inventory Logo' /> Accessory Inventory </a> <br>";
         echo "<a class='{$classes['accessorytype']}' href='AccessoryTypes.php'> <img src='img/icons/accessorytypes.png' alt='Bike Types Logo' /> Accessory Types </a> <br>";
         echo "<a class='{$classes['bookings']}' href='bookings.php'> <img src= 'img/icons/book-open-blank-variant.png' alt='Bookings Logo' /> Bookings </a> <br>";
         echo "<a class='{$classes['blockoutdate']}' href='Block_Out_Date.php'> <img src= 'img/icons/calendar.png' alt='Block out date Logo' /> Block Out Dates </a> <br>";
