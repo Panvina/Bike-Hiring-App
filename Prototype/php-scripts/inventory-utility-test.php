aadc2abda28bb9456ff3eb455ba708c3208868e4
aadesh-j <62002089+aadesh-j@users.noreply.github.com>
2022-10-17 21:46:52 +1100

    Inventory pages validation function testing

---
+++ Prototype/php-scripts/inventory-utility-test.php
@@ -0,0 +1,70 @@
+<?php
+include("inventory-util.php");
+
+$actual_result = array("");
+$expected_result = array("");
+$test_case = array("");
+$result = array("");
+
+test_name_validation();
+test_price_validation();
+
+function assert_match($expected, $actual)
+{
+    if($expected == $actual)
+    {
+        return "Passed!";
+    }
+    else
+    {
+        return "Failed!";
+    }
+}
+
+//Testing the name validation regex function
+function test_name_validation()
+{
+    //Incorrect value test
+    $expected_result[0] = 0;
+    $test_case[0] = "./231a";
+
+    $actual_result[0] = validName($test_case[0]);
+    $result[0] = assert_match($expected_result[0],$actual_result[0]);
+
+    echo "Test 1 - Name Validation || Actual: $actual_result[0] || Expected: $expected_result[0] || Result:  $result[0]";
+
+    //Correct value test
+    $expected_result[1] = 1;
+    $test_case[1] = "Mountain Bike - 1";
+
+    $actual_result[1] = validName($test_case[1]);
+    $result[1] = assert_match($expected_result[1],$actual_result[1]);
+
+    echo "<br>"."Test 2 - Name Validation || Actual: $actual_result[1] || Expected: $expected_result[1] || Result:  $result[1]";
+}
+
+//Testing the price validation regex function
+function test_price_validation()
+{
+    //Incorrect value test
+    $expected_result[2] = 0;
+    $test_case[2] = "twenty-four";
+
+    $actual_result[2] = validPrice($test_case[2]);
+
+    $result[2] = assert_match($expected_result[2],$actual_result[2]);
+
+
+    echo "<br>"."Test 3 - Price Validation || Actual: $actual_result[2] || Expected: $expected_result[2] || Result:  $result[2]";
+
+    //Correct value test
+    $expected_result[3] = 1;
+    $test_case[3] = "24.00";
+
+    $actual_result[3] = validPrice($test_case[3]);
+
+    $result[3] = assert_match($expected_result[3],$actual_result[3]);
+
+    echo "<br>"."Test 4 - Price Validation || Actual: $actual_result[3] || Expected: $expected_result[3] || Result:  $result[3]";
+}
+?>
